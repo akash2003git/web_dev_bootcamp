24dc775f91203fc4c9a576abbebf4c9c6bf04a29
akash2003git <akash.tayade.dev@gmail.com>
2024-12-25 18:14:40 +0530

    Updates

---
+++ npm/require_directory/shelter/index.js
@@ -0,0 +1,7 @@
+const blue = require("./blue");
+const saddie = require("./saddie");
+const janet = require("./janet");
+
+const allCats = [blue, saddie, janet];
+//console.log(allCats);
+module.exports = allCats;
